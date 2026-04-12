7c5b9d60e7baa553951c4955fa1bf23d41294e8c
TanmayGaidhani <tanmaygaidhani3@gmail.com>
2026-04-12 10:59:30 +0530

    fix: use CompressedStaticFilesStorage only in production

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -73,7 +73,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+
+# Use compressed storage only in production
+if not DEBUG:
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
